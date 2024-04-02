08b2e5874bb1308a770a5e11e7bf661ae2c423a2
nosovegor178 <nosovbelakovegor01@gmail.com>
2024-04-02 20:11:31 +0300

    Removed mistakes

--- e-diary.py
+++ e-diary.py
@@ -3,26 +3,40 @@ from datacenter.models import Mark
 from datacenter.models import Chastisement
 from datacenter.models import Lesson
 from datacenter.models import Commendation
+from django.shortcuts import get_list_or_404
+from django.shortcuts import get_object_or_404
 stud_surname_and_name = "Фролов Иван"
 subject = "Математика"
+commendation = "Хвалю!"
+
+
 def get_student(stud_full_name):
-    students_list = Schoolkid.objects.filter(full_name__contains=stud_surname_and_name)
+    student_list = get_list_or_404(Schoolkid, full_name__contains=stud_full_name)
     if len(student_list) > 1:
-        print('There are {} students with that name. Please specify full name.'.format(len(student_list)))
-    elif len(student_list) == 0:
-        print('There are no students with such name.')
+        print('''There are {} students with that name.
+            Please specify full name.'''.format(len(student_list)))
     else:
-        return(student_list[0])
-def fix_marks(schoolkid):
-    stud_bad_marks = Mark.objects.filter(schoolkid=student, points__in=[2, 3])
-    for bad_mark in stud_bad_marks:
-        bad_mark.points=5
-        bad_mark.save()
-def remove_chastisements(schoolkid):
+        return student_list[0]
+
+
+def fix_marks(student):
+    Mark.objects.filter(schoolkid=student, points__in=[2, 3]).update(points=5)
+
+
+def remove_chastisements(student):
     stud_chastisements = Chastisement.objects.filter(schoolkid=student)
     stud_chastisements.delete()
-def create_commendation(student, lesson):
-    all_stud_lessons = Lesson.objects.filter(year_of_study=student.year_of_study, group_letter=student.group_letter)
-    all_stud_needed_lessons = all_stud_lessons.filter(subject__title__contains=lesson)
-    last_lesson = all_stud_needed_lessons.order_by('date').first()
-    Commendation.objects.create(text="Хвалю!", created=last_lesson.date, schoolkid=student, subject=last_lesson.subject, teacher=last_lesson.teacher)
+
+
+def create_commendation(student, lesson, commendation):
+    stud_needed_lessons = get_object_or_404(Lesson,
+        year_of_study=student.year_of_study,
+        group_letter=student.group_letter,
+        subject__title__contains=lesson)
+    last_lesson = stud_needed_lessons.order_by('-date').first()
+    Commendation.objects.create(
+        text=commendation,
+        created=last_lesson.date,
+        schoolkid=student,
+        subject=last_lesson.subject,
+        teacher=last_lesson.teacher)
