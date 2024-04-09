f77ab3a833d61ad5cef3517d963fd2d5816e7f80
nosovegor178 <nosovbelakovegor01@gmail.com>
2024-04-09 18:23:23 +0300

    Update file

--- e-diary.py
+++ e-diary.py
@@ -5,18 +5,11 @@ from datacenter.models import Lesson
 from datacenter.models import Commendation
 from django.shortcuts import get_list_or_404
 from django.shortcuts import get_object_or_404
-stud_surname_and_name = "Фролов Иван"
-subject = "Математика"
-commendation = "Хвалю!"
 
 
 def get_student(stud_full_name):
-    student_list = get_list_or_404(Schoolkid, full_name__contains=stud_full_name)
-    if len(student_list) > 1:
-        print('''There are {} students with that name.
-            Please specify full name.'''.format(len(student_list)))
-    else:
-        return student_list[0]
+    student_list = get_object_or_404(Schoolkid, full_name__contains=stud_full_name)
+    return student_list
 
 
 def fix_marks(student):
@@ -29,11 +22,15 @@ def remove_chastisements(student):
 
 
 def create_commendation(student, lesson, commendation):
-    stud_needed_lessons = get_object_or_404(Lesson,
+    stud_needed_lessons = Lesson.objects.filter(
         year_of_study=student.year_of_study,
         group_letter=student.group_letter,
         subject__title__contains=lesson)
-    last_lesson = stud_needed_lessons.order_by('-date').first()
+    if stud_needed_lessons is None:
+        print('There is no such lesson!')
+        break()
+    else:
+        last_lesson = stud_needed_lessons.order_by('-date').first()
     Commendation.objects.create(
         text=commendation,
         created=last_lesson.date,
